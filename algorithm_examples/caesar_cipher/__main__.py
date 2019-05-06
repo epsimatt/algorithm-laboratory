d9388e7a8ee19d888f044fd200995bcedb0690f8
epsimatt <epsimatt@protonmail.com>
2019-05-06 12:53:43 +0900

    update: caesar_cipher

--- algorithm_examples/caesar_cipher/__main__.py
+++ algorithm_examples/caesar_cipher/__main__.py
@@ -14,7 +14,10 @@ def caesar_cipher_idx1(plain_text: str, shift: int) -> str:
     result = ""
 
     for i in range(len(plain_text)):
-        index = ord(plain_text[i]) - ord('A') + 1 # 1 <= index <= 26
+        index = ord(plain_text[i]) - ord('A') + 1
+
+        # 문자열의 index는 1부터 26까지의 값이 되어야 한다.
+        # 따라서 이동 거리가 `shift - 1`인 카이사르 단어의 각 문자의 index를 구해 1을 더해야 한다.
         result += " ABCDEFGHIJKLMNOPQRSTUVWXYZ"[((index + (shift - 1)) % 26) + 1]
 
     return result
@@ -36,16 +39,19 @@ def caesar_decipher_idx1(cipher_text: str, shift: int) -> str:
     result = ""
 
     for i in range(len(cipher_text)):
-        index = ord(cipher_text[i]) - ord('A') + 1 # 1 <= index <= 26
+        index = ord(cipher_text[i]) - ord('A') + 1
+
+        # 문자열의 index는 1부터 26까지의 값이 되어야 한다.
+        # 따라서 이동 거리가 `shift + 1`일 때 평문의 각 문자의 index를 구해 1을 더해야 한다.
         result += " ABCDEFGHIJKLMNOPQRSTUVWXYZ"[((index - (shift + 1)) % 26) + 1]
 
     return result
 
+
 if __name__ == '__main__':
-    encrypted_message = caesar_cipher('HELLO', 3)
-    decrypted_message = caesar_decipher('KHOOR', 3)
 
-    print(f"caesar_cipher(): {encrypted_message}")
-    print(f"caesar_decipher(): {decrypted_message}")
+    assert caesar_cipher('HELLO', 3) == 'KHOOR'
+    assert caesar_cipher_idx1('WXYZ', 3) == 'ZABC'
 
-    pass
+    assert caesar_decipher('KHOOR', 3) == 'HELLO'
+    assert caesar_decipher_idx1('ZABC', 3) == 'WXYZ'
