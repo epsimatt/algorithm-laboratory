f63df4d1fb4ea8d7ec098fd68c3827779e7eb2e5
epsimatt <epsimatt@protonmail.com>
2019-06-23 11:54:21 +0900

    update: maximum_subarray

--- lc_solutions/arrays_and_strings/maximum_subarray-53/__main__.py
+++ lc_solutions/arrays_and_strings/maximum_subarray-53/__main__.py
@@ -1,24 +1,47 @@
 from typing import List
 
 
-# runtime: 99.62%, memory usage: 62.69%
-def maxSubArray(nums: List[int]) -> int:
-    best = nums[0]
-    sum = 0
+# time limit exceeded
+def maxSubArray_v1(nums: List[int]) -> int:
+    n = len(nums)
 
-    for i in range(0, len(nums)):
-        if nums[i] < sum + nums[i]:
-            sum += nums[i]
+    if n == 0:
+        return 0
+    elif n == 1:
+        return nums[0]
+    else:
+        res = -float('inf')
+
+        for i in range(0, n):
+            for j in range(i, n):
+                sub_res = sum(nums[i:j + 1])
+
+                if res < sub_res:
+                    res = sub_res
+
+        return res
+
+
+# runtime: 99.61%, memory usage: 84.99%
+def maxSubArray_v2(nums: List[int]) -> int:
+    n = len(nums)
+
+    result, sub_res = -float('inf'), 0
+
+    for i in range(0, n):
+        if sub_res + nums[i] > nums[i]:
+            sub_res += nums[i]
         else:
-            sum = nums[i]
+            sub_res = nums[i]
 
-        if best < sum:
-            best = sum
+        if result < sub_res:
+            result = sub_res
 
-    return best
+    return result
 
 
 if __name__ == '__main__':
-    result = maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
-
-    print(result)
+    print(maxSubArray_v2([-1])) # -1
+    print(maxSubArray_v2([1])) # 1
+    print(maxSubArray_v2([-2, 1]))  # 1
+    print(maxSubArray_v2([-2, 1, -3, 4, -1, 2, 1, -5, 4])) # 6 ([4, -1, 2, 1])
