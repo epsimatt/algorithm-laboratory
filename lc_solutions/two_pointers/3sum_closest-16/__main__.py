1bd9acac68ddad5a9927945de70405e61664c11f
epsimatt <epsimatt@protonmail.com>
2019-07-13 23:15:17 +0900

    update: 3sum_closest

--- lc_solutions/two_pointers/3sum_closest-16/__main__.py
+++ lc_solutions/two_pointers/3sum_closest-16/__main__.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-# runtime: 11.46%, memory usage: 30.00%
+# runtime: 21.83%, memory usage: 54.23%
 def threeSumClosest_v1(nums: List[int], target: int) -> int:
     nums.sort()
 
@@ -10,6 +10,9 @@ def threeSumClosest_v1(nums: List[int], target: int) -> int:
     result = float('inf')
 
     for i in range(n - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
         low, high = i + 1, n - 1
 
         while low < high:
@@ -21,17 +24,49 @@ def threeSumClosest_v1(nums: List[int], target: int) -> int:
             if abs(diff_rs) > abs(diff_ts):
                 result = three_sum
 
-            if diff_ts > 0:
-                high -= 1
-            else:
+            if three_sum == target:
+                return three_sum
+            elif three_sum < target:
                 low += 1
+            else:
+                high -= 1
 
     return result
 
 
-# runtime: %, memory usage: %
+# runtime: 88.58%, memory usage: 23.07%
 def threeSumClosest_v2(nums: List[int], target: int) -> int:
-    pass
+    nums.sort()
+
+    result = float('inf')
+
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        low, high = i + 1, len(nums) - 1
+
+        while low < high:
+            three_sum = nums[i] + nums[low] + nums[high]
+
+            diff_ts = three_sum - target
+            diff_rs = result - target
+
+            if abs(diff_rs) > abs(diff_ts):
+                result = three_sum
+
+            if three_sum == target:
+                return three_sum
+            elif three_sum < target:
+                while low < high and nums[low] == nums[low + 1]:
+                    low += 1
+                low += 1
+            else:
+                while low < high and nums[high] == nums[high - 1]:
+                    high -= 1
+                high -= 1
+
+    return result
 
 
 if __name__ == '__main__':
